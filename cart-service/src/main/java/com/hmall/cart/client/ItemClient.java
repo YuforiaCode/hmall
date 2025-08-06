42bc7a162884a230f2028c4b46177b4419ad36e2
YuforiaCode <2628489679@qq.com>
2025-08-06 10:24:32 +0800

    OpenFeign-快速入门

---
+++ cart-service/src/main/java/com/hmall/cart/client/ItemClient.java
@@ -0,0 +1,15 @@
+package com.hmall.cart.client;
+
+import com.hmall.cart.domain.dto.ItemDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+@FeignClient("item-service")  //声明服务的名字
+public interface ItemClient {
+    @GetMapping("/items")  //请求方式、请求路径
+    List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);  //返回值类型、请求参数
+}
--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.client.ItemClient;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
@@ -43,9 +44,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-    private final RestTemplate restTemplate;
+    /*private final RestTemplate restTemplate;
 
-    private final DiscoveryClient discoveryClient;
+    private final DiscoveryClient discoveryClient;*/
+
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -89,10 +92,10 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     }
 
     private void handleCartItems(List<CartVO> vos) {
-        //TODO 1.获取商品id
+        // 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
-        // 2.1.根据服务名称获取服务列表
+        /*// 2.1.根据服务名称获取服务列表
         List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
         if (CollUtils.isEmpty(instances)) {
             return;
@@ -113,7 +116,8 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
             //查询失败，结束
             return;
         }
-        List<ItemDTO> items = response.getBody();
+        List<ItemDTO> items = response.getBody();*/
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
         if (CollUtils.isEmpty(items)) {
             return;
         }
