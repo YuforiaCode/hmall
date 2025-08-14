fad51d04e580c9cc9116b55dcbe3e44c75cf0356
YuforiaCode <2628489679@qq.com>
2025-08-14 18:48:51 +0800

    JavaRestClient-文档的CRUD

--- item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
+++ item-service/src/test/java/com/hmall/item/es/ElasticDocumentTest.java
@@ -6,7 +6,12 @@ import com.hmall.item.domain.po.Item;
 import com.hmall.item.domain.po.ItemDoc;
 import com.hmall.item.service.IItemService;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -26,6 +31,9 @@ public class ElasticDocumentTest {
     @Autowired
     private IItemService itemService;
 
+    /**
+     * 新增文档(id存在时就是修改文档的全量更新)
+     */
     @Test
     void testIndexDocument() throws IOException {
         //0.准备文档数据
@@ -34,12 +42,56 @@ public class ElasticDocumentTest {
         //0.2.将数据库数据转为文档数据
         ItemDoc itemDoc = BeanUtil.copyProperties(item, ItemDoc.class);
 
+        itemDoc.setPrice(29900);
+
         //1.准备request
         IndexRequest request = new IndexRequest("items").id(itemDoc.getId());
         //2.准备请求参数
         request.source(JSONUtil.toJsonStr(itemDoc), XContentType.JSON);
         //3.发送请求
-        client.index(request, RequestOptions.DEFAULT);
+        IndexResponse resp = client.index(request, RequestOptions.DEFAULT);
+        System.out.println("resp = " + resp);
+    }
+
+    /**
+     * 查询文档
+     */
+    @Test
+    void testGetDocument() throws IOException {
+        //1.准备request
+        GetRequest request = new GetRequest("items", "100000011127");
+        //2.发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        //3.解析响应中的source
+        String json = response.getSourceAsString();
+        ItemDoc doc = JSONUtil.toBean(json, ItemDoc.class);
+        System.out.println("doc = " + doc);
+    }
+
+    /**
+     * 删除文档
+     */
+    @Test
+    void testDeleteDocument() throws IOException {
+        //1.准备request
+        DeleteRequest request = new DeleteRequest("items", "100000011127");
+        //2.发送请求
+        client.delete(request, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 修改文档(局部更新)
+     */
+    @Test
+    void testUpdateDocument() throws IOException {
+        //1.准备request
+        UpdateRequest request = new UpdateRequest("items", "100000011127");
+        //2.准备请求参数
+        request.doc(
+                "price", 25600
+        );
+        //3.发送请求
+        client.update(request, RequestOptions.DEFAULT);
     }
 
     @BeforeEach
