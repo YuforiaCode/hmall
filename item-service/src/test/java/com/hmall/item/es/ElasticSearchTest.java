0946f9442771fabe22d28d1724d2ccb960c968c2
YuforiaCode <2628489679@qq.com>
2025-08-19 21:12:17 +0800

    JavaRestClient查询-构建查询条件

--- item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
+++ item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
@@ -1,36 +1,21 @@
 package com.hmall.item.es;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.json.JSONUtil;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.hmall.item.domain.po.Item;
 import com.hmall.item.domain.po.ItemDoc;
-import com.hmall.item.service.IItemService;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.util.List;
 
 //@SpringBootTest(properties = {"spring.profiles.active=local", "seata.enabled=false"})
 public class ElasticSearchTest {
@@ -38,7 +23,7 @@ public class ElasticSearchTest {
     private RestHighLevelClient client;
 
     /**
-     * 测试查询所有
+     * MatchAll查询所有(JavaRestClient快速入门)
      */
     @Test
     void testMatchAll() throws IOException {
@@ -51,6 +36,34 @@ public class ElasticSearchTest {
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 
         //4.解析结果
+        parseResponseResult(response);
+    }
+
+    /**
+     * 构建复杂查询条件的搜索
+     */
+    @Test
+    void testSearch() throws IOException {
+        //1.创建request对象
+        SearchRequest request = new SearchRequest("items");
+        //2.组织DSL参数
+        request.source().query(
+                QueryBuilders.boolQuery()
+                        .must(QueryBuilders.matchQuery("name", "脱脂牛奶"))
+                        .filter(QueryBuilders.termQuery("brand", "德亚"))
+                        .filter(QueryBuilders.rangeQuery("price").lt(10000))
+        );
+        //3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        //4.解析结果
+        parseResponseResult(response);
+    }
+
+    /**
+     * 封装解析结果的api代码
+     */
+    private static void parseResponseResult(SearchResponse response) {
         SearchHits searchHits = response.getHits();
         //4.1.总条数
         long total = searchHits.getTotalHits().value;
